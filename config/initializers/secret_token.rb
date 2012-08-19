5337342bed38073e11c9480384497bb01b098c3d
Emily Ellison <emilyellison986@gmail.com>
2012-08-19 10:51:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerStore::Application.config.secret_token = '0e5ccc355fad8980a6cdec8615fac59ec7f8b81410a8a5e6aac4b04d555755ff612bc2c68fdfc8e90f63c7b741adce03ea2f978f757fc80479dcdc45fda1b654'
