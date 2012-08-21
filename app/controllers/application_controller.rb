ab2b7ce25cadfb7ef5ef74e436f0bbd4f79f1c1d
Emily Ellison <emilyellison986@gmail.com>
2012-08-21 10:15:16 -0500

    Add authorized to change method in application controller that can also be used as a helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,18 @@ class ApplicationController < ActionController::Base
   
   before_filter :find_logged_in_user
   
+  helper_method :authorized_to_change
+  
   def find_logged_in_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
   
+  def authorized_to_change(user_id)
+    @current_user.present? && @current_user.id == user_id
+  end
+  
   def redirect_if_not_authorized(user_id)
-    if @current_user.nil? || @current_user.id != user_id
+    if !authorized_to_change(user_id)
       flash[:error] = 'You were not authorized to perform that action.'
       redirect_to :back
     end
