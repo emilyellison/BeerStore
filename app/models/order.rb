9acb1be6b78e779ab346d7ca9a0128fe593d9353
Emily Ellison <emilyellison986@gmail.com>
2012-08-24 09:16:53 -0500

    Add order model

---
+++ app/models/order.rb
@@ -0,0 +1,3 @@
+class Order < ActiveRecord::Base
+  attr_accessible :card_expires_on, :card_type, :cart_id, :first_name, :ip_address, :last_name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120821145535) do
+ActiveRecord::Schema.define(:version => 20120824132425) do
 
   create_table "beers", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,17 @@ ActiveRecord::Schema.define(:version => 20120821145535) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "orders", :force => true do |t|
+    t.integer  "cart_id"
+    t.string   "ip_address"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "card_type"
+    t.date     "card_expires_on"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "users", :force => true do |t|
