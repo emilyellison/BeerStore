c01baae61656ab99ac728eda758ae8e00d964f8f
Emily Ellison <emilyellison986@gmail.com>
2012-08-30 12:56:27 -0500

    Add express route and set up paypal checkout express button

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ BeerStore::Application.routes.draw do
   resources :beers
   resources :users, only: [ :new, :create, :show ]
   resources :cart_items, only: [ :create, :destroy ]
-  resources :orders, only: [ :new, :create ]
+  resources :orders, only: [:new, :create] do
+  get 'express', :on => :collection
+  end
   
   get '/sign_in' => 'sessions#new', as: :sign_in
   post '/sign_in' => 'sessions#create'
